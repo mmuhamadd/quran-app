fe78b6e447c70fd5929580213e61a4883fde0b8f
mmuhamadd <106642318+mmuhamadd@users.noreply.github.com>
2024-03-19 02:24:51 +0200

    Update Reciter.vue

--- src/views/Reciter.vue
+++ src/views/Reciter.vue
@@ -29,7 +29,7 @@ export default {
 	},
 	mounted(){
 		const reciterId = this.$route.params.name;
-		axios.get("../src/json/reciters.json")
+		axios.get("https://github.com/mmuhamadd/quran-app/blob/main/src/json/reciters.json")
 		.then((res)=>{
 			const reciters = res.data;
 			reciters.forEach((reciter)=>{
