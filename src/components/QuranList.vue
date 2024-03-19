3866dbd1e45b42ec7e4e72ae2d20090b573848b6
mmuhamadd <106642318+mmuhamadd@users.noreply.github.com>
2024-03-19 20:18:59 +0200

    Update QuranList.vue

--- src/components/QuranList.vue
+++ src/components/QuranList.vue
@@ -1,18 +1,18 @@
 <template>
-	<div id="quran-list">
-		<h1 class="text-white text-3xl font-bold">استماع للقراءن بصوت القارئ:</h1>
-		<div class="flex flex-wrap gap-5 p-5">
-			<router-link v-for="surah in surahs" class="p-3 w-[47%] lg:w-[31%] bg-dark-2 rounded-2xl cursor-pointer flex items-center justify-center shadow shadow-2xl" :to="`/surah/${surah.id}/${currentReciter}`">
-				<h3>{{ surah.name }}</h3>
+	<div id="quran-list" class="p-5 rtl">
+		<h1 class="text-white text-3xl font-bold mb-5">استماع للقراءن بصوت القارئ:</h1>
+		<div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+			<router-link v-for="surah in surahs" :key="surah.id" class="p-3 bg-dark-2 rounded-2xl cursor-pointer flex items-center justify-center shadow-2xl" :to="`/surah/${surah.id}/${currentReciter}`">
+				<h3 class="text-white">{{ surah.name }}</h3>
 			</router-link>
 		</div>
 	</div>
 </template>
 
-<style type="text/css" scoped>
-	#quran-list {
-		@apply p-5;
-	}
+<style scoped>
+#quran-list {
+	@apply p-5 w-screen;
+}
 </style>
 
 <script type="text/javascript">
