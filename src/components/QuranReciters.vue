8b172347af53a53a0d5a1219f3861c2e3007e4bd
mmuhamadd <106642318+mmuhamadd@users.noreply.github.com>
2024-03-19 21:31:15 +0200

    Update QuranReciters.vue

--- src/components/QuranReciters.vue
+++ src/components/QuranReciters.vue
@@ -5,7 +5,7 @@
     </h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-5">
       <router-link v-for="reciter in getReciters" :to="`/reciter/${reciter.id}`" :key="reciter.id" class="p-3 bg-dark-2 rounded-2xl cursor-pointer flex flex-col items-center justify-center shadow-2xl">
-        <img :src="${reciter.picture}" width="80" height="80" class="rounded-full object-cover h-20 w-20 mb-3" loading="lazy" :alt="reciter.name" :title="reciter.name">
+        <img :src="reciter.picture" width="80" height="80" class="rounded-full object-cover h-20 w-20 mb-3" loading="lazy" :alt="reciter.name" :title="reciter.name">
         <p class="text-white font-medium text-center">{{ reciter.name }}</p>
       </router-link>
     </div>
